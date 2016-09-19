f7176c2005bba6e8ff9896de54a86cbe02e89c84
William Speirs <bill.speirs@gmail.com>
2016-09-19 15:35:07 -0400

    Got WALIterator compiling

--- src/record_iterator.rs
+++ src/record_iterator.rs
@@ -1,6 +1,7 @@
 extern crate bincode;
 extern crate rustc_serialize;
 
+use bincode::rustc_serialize::{encode, decode};
 use rustc_serialize::{Encodable, Decodable};
 
 use ::{KeyType, ValueType};
@@ -8,22 +9,59 @@ use ::{KeyType, ValueType};
 use std::collections::{BTreeMap, BTreeSet};
 use std::collections::btree_map::Iter as MIter;
 use std::collections::btree_set::Iter as SIter;
+use std::error::Error;
+use std::fs::{File, OpenOptions};
+use std::io::Read;
 use std::iter::Peekable;
+use std::marker::PhantomData;
 
 #[derive(RustcEncodable, RustcDecodable, PartialEq)]
-struct WALRecord<K: KeyType, V: ValueType> {
+struct KeyValuePair<K: KeyType, V: ValueType> {
     key: K,
     value: V,
 }
 
-impl <K: KeyType, V: ValueType> Iterator for WALRecord<K,V> {
-    type Item = WALRecord<K,V>;
+struct WALIterator<K: KeyType, V: ValueType> {
+    fd: File,  // the WAL file
+    key_size: usize,
+    value_size: usize,
+    _k_marker: PhantomData<K>,
+    _v_marker: PhantomData<V>
+}
+
+impl <K: KeyType, V: ValueType> WALIterator<K,V> {
+    fn new(wal_file_path: String, key_size: usize, value_size: usize) -> Result<WALIterator<K,V>, Box<Error>> {
+        let mut wal_file = try!(OpenOptions::new().read(true).create(false).open(wal_file_path));
+
+        return Ok(WALIterator{fd: wal_file,
+                              key_size: key_size,
+                              value_size: value_size,
+                              _k_marker: PhantomData,
+                              _v_marker: PhantomData});
+    } 
+}
+
+impl <K: KeyType, V: ValueType> Iterator for WALIterator<K,V> {
+    type Item = KeyValuePair<K,V>;
 
     fn next(&mut self) -> Option<Self::Item> {
+        let total_size = self.key_size + self.value_size;
+        let mut buff = vec![0; total_size];
 
+        // attempt to read a buffer's worth and decode
+        match self.fd.read_exact(&mut buff) {
+            Ok(_) => {
+                match decode(&buff) {
+                    Ok(record) => Some(record),
+                    Err(e) => None
+                }
+            },
+            Err(e) => None
+        }
     }
 }
 
+/*
 
 struct MemoryRecordIterator<'a, 'b, K: KeyType, V: ValueType> {
     key_size: usize,
@@ -51,5 +89,5 @@ impl <'a, 'b, K: KeyType, V: ValueType> Iterator for MemoryRecordIterator<'a, 'b
     }
 }
 
-
+*/
 
